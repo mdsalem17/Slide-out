f12745e0175710b2944aed14810fc24ab122d604
ABDRABO <khaled.abdrabo@etu.univ-lyon1.fr>
2019-03-19 16:58:46 +0100

    affichage Perlin

--- src/grapic/mainGrapic.cpp
+++ src/grapic/mainGrapic.cpp
@@ -8,77 +8,52 @@
 #include <unistd.h>
 #endif // WIN32
 
+#include "../core/Perlin.h"
+
 using namespace grapic;
 
 const int DIMW = 700 ;
 const int DIM = 9;
 const int DIM_SPRITE = 32;
 
-void init(Game & game)
+void init(Game & game, Perlin & perlin)
 {
+    game.getPlayer().setPosition(b2Vec2(DIMW/2, DIMW/2),0);
     game.getPlayer().applyForce(b2Vec2(250,35));
 }
 
-void draw(const Game & game, float & range)
+void draw(const Game & game, Perlin perlin, float & range)
 {
-    color(255,0,0);
-    circleFill(game.getPlayer().getPosition().x, -game.getPlayer().getPosition().y, 5);
-
-    // => pour le tout le tableau 
-    for(int i = range ; i < 30+range ; i++)
-    {
-        float32 x1,x2,y1,y2 ;
-        x1 = 10*(game.getTerrain().tabHeight.at(i).x) - range*50;
-        x2 = 10*(game.getTerrain().tabHeight.at(i+1).x) - range*50 ;
-        y1 = 150+game.getTerrain().tabHeight.at(i).y;
-        y2 = 150+game.getTerrain().tabHeight.at(i+1).y;
-        color(255, 255, 255);
-        circleFill(x1, y1, 3);
-        circleFill(x2, y2, 3);
-        line( x1, y1, x2, y2 );
-    }
-
-    //isKeyPressed(SDLK_RIGHT) &&
-    if (range < game.getTerrain().getnbPts()-30)
-    {
-        if(isKeyPressed(SDLK_SPACE))
-        {
-            #ifdef _WIN32
-            Sleep(100);
-            #else
-            usleep(100000); //0.1 sec
-            #endif // WIN32
-            range = range + 0.05f ;
-        }
-        #ifdef _WIN32
-        Sleep(100);
-		#else
-		usleep(100000); //0.1 sec
-        #endif // WIN32
-        range++;
+    color(255,255,0);
+    int radius = 5 ;
+    circleFill(game.getPlayer().getPosition().x + radius, game.getPlayer().getPosition().y+radius, radius);  
+    
+    for(int i = 1 ; i < perlin.getNbPts() ; i++){
+        line(i-1, perlin.getPtsPerlin(i-1).y, i, perlin.getPtsPerlin(i).y);
     }
 }
 
-void update(Game & game)
+void update(Game & game, Perlin & perlin)
 {   
     game.updateBox2dWorld();
+    //appel updatePerlin();
 }
-
 
 int main(int , char ** )
 {
-    float part = 0; 
-	Game game ;
+    float part = 0; 	
+    Game game ;
+    Perlin perlin;
     bool stop=false;
 	winInit("SlideOut", DIMW*2,DIMW);
     setKeyRepeatMode(true); 
-    init(game);
+    init(game, perlin);
 	while( !stop )
     {
         backgroundColor( 100, 80, 200, 255 );
         winClear();
-        update(game);
-        draw(game,part);     
+        update(game, perlin);
+        draw(game, perlin, part);     
         stop = winDisplay();
     }
     winQuit();
