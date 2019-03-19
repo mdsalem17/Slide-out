cc83372c46e18f92cca4961722ed551c820098fc
ABDRABO <khaled.abdrabo@etu.univ-lyon1.fr>
2019-03-19 17:00:52 +0100

    ajout classe Perlin

---
+++ src/core/Perlin.h
@@ -0,0 +1,35 @@
+#ifndef PERLIN_H
+#define PERLIN_H
+
+#include <Box2D/Box2D.h>
+#include <vector>
+#include <math.h>
+
+class Perlin
+{
+    public:
+    Perlin();
+    Perlin(int _octaves, int _zoomX, int _zoomY, int _offset, double _persistance, double _frequency, int _nbPts);
+    ~Perlin();
+    int getNbPts() const;
+    void updatePerlin(int nb);
+    b2Vec2 getPtsPerlin(int i);
+        std::vector<b2Vec2> tabPerlin;
+        void initPerlin();
+
+
+    private:
+    int nbPts;
+    //std::vector<double> tabPerlin;
+    int octaves, zoomX, zoomY, offset ;
+    double persistance, frequency;
+
+    //configuration Perlin
+	double rand_noise(int t);
+	double cosine_interp(double a, double b, double t);
+	double interp_noise(double x);
+	double perlin(int octaves, double frequency, double persistance, double x);
+    	
+};
+
+#endif
