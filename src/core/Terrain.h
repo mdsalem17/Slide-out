b854791a19b2077b311c07bb413c2ce7f8075efd
ABDRABO <khaled.abdrabo@etu.univ-lyon1.fr>
2019-03-19 17:02:28 +0100

    ajout Perlin et gestion des collisions

--- src/core/Terrain.h
+++ src/core/Terrain.h
@@ -1,32 +1,24 @@
 #ifndef TERRAIN_H
 #define TERRAIN_H
 
-#include <Box2D/Box2D.h>
-#include <iostream>
 #include <vector>
-
+#include <Box2D/Box2D.h>
+#include "Perlin.h"
 
 class Terrain {
     public:
+
 	Terrain();
-	
-	std::vector<b2Vec2> tabHeight;
-	unsigned int nbPts;
-	unsigned int getnbPts() const;
+	~Terrain();
 	void initTerrain(b2World *w);
 
-	~Terrain();
     private:
-	b2World *world;
-    b2Body  *terrainBody;
-
 	float slop;
-	unsigned int pixelScale;
+	b2World *world;
+	b2Body *terrainBody;
+	Perlin perlin;
 	
-	float getHeight() const;
 	float getSlop() const;
-
-
 };
 
 #endif
