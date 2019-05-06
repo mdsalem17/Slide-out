af776f225b1f2b887c294b1829b75faa318d30d7
MESSOUD <mohamed-salem.messoud@etu.univ-lyon1.fr>
2019-05-06 15:08:42 +0200

    ajout de commentaires et structuration

--- src/core/Game.h
+++ src/core/Game.h
@@ -20,13 +20,51 @@
 
 /** \defgroup moduleGame module de getsion du jeu
  *
- * \brief chef d'orshestre qui  permet de gerer les interactions entre les differents objets du jeu(terrain/player).
+ * \brief chef d'orshestre qui permet de gerer les interactions entre les differents objets du jeu(terrain/player).
  */
   
 class Game
 {
     public:
- 
+     
+        /**
+          * @brief dimension de la fenetre en x
+          *
+        */
+        int dimx;
+        /**
+          * @brief dimension de la fenetre en y
+          *
+        */
+        int dimy;
+
+        /**
+          * @brief variable contenant le score
+          *
+        */
+        int score;
+        /**
+          * @brief variable contenant le nombre de bonus récupéré dans le jeu
+          *
+        */
+        int bonus_score;
+        
+        /**
+          * @brief variable contenant le vecteur de la force de la gravité
+          *
+        */
+        b2Vec2 gravity;
+        /**
+          * @brief variable contenant le monde de Box2D
+          *
+        */
+        b2World* world;
+        /**
+          * @brief variable contenant le tableau des bonus
+          *
+        */
+        std::vector<b2Vec2> BonusPoints;
+
     /**
         @brief Constructeur par défaut de la classe Game: initialise dimx et dimy
                 ce constructeur n'alloue pas de World
@@ -34,35 +72,46 @@ class Game
     */
         Game();
 
-    /**
-       @brief destructeur de la classe Game
-    */
-        ~Game();
-
-        int dimx;
-        int dimy;
-
-        int score;
-        int bonus_score;
-        
-        b2Vec2 gravity;
-        b2World* world;
-
+        /**
+            @brief fonction qui determine si le niveau est terminé
+        */
         bool isLevelFinished();
-        void initBox2dWorld(const b2Vec2 &gravity);
-        void updateBox2dWorld();
-        
-        void addBonusPoints();
-        std::vector<b2Vec2> BonusPoints;
 
+        /**
+            \brief fonction qui initialise la donnée membre world
+            \param gravity: la force de gravité appliquée sur le monde
+        */
+        void initBox2dWorld(const b2Vec2 &gravity);
+
+        /**
+            \brief procédure qui met à jour le monde
+        */
+        void updateBox2dWorld();
+        /**
+            \brief procédure qui permet d'ajouter des bonus tout au long du terrain
+        */
+        void addBonusPoints();
+        /**
+            \brief procédure qui de gérer les collisions du joueur et d'agmenter le score en fonctions des pentes
+        */
         void collision();
+        /**
+            \brief fonction qui détruit les collisions du terrain et le tableau contenant les points à la fin du niveau
+        */
         void destroyTerrain();
-        b2Vec2 &getRelativePlayerPos(); 
+        /**
+            @brief fonction qui permet de récupérer la position relative du joueur 
+        */
+        b2Vec2 &getRelativePlayerPos();
+        
+        /**
+            @brief fonction qui permet de récupérer la position relative du Terrain 
+        */
         b2Vec2 &getRelativeTerrainPos(); 
 
     /**
-     *     @brief Accesseur : permet d'acceder à l'objet player instancié par cette classe
-     * \return Terrain
+     *     \brief Accesseur : permet d'acceder à l'objet Player instancié par cette classe
+     *     \return Terrain
     */
         Player* getPlayer();
 
@@ -83,6 +132,11 @@ class Game
             void BeginContact(b2Contact* contact);
             void EndContact(b2Contact* contact);
         };
+
+    /**
+       @brief destructeur de la classe Game
+    */
+        ~Game();
 
     private:
         Player *player;
